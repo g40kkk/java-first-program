196faceb660d8090792b31c94440a62b46c6ac82
Kun Kaewken <kunkk01@gmail.com>
2020-09-09 16:52:26 -0500

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -17,7 +17,7 @@ public class BestLoanRates {
         if (bestRate == 0.0f) {
             System.out.println("No available rates for term: " + loanTermInYears + " years");
         } else {
-            System.out.println("Best Available Rate: : " + getRates(loanTermInYears) + "%");
+            System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
         }
         scanner.close();
     }
